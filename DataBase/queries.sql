55aa8c0131152e42b07e636b9588489578f67daa
LautiGraziano <lautygraziano@gmail.com>
2026-09-10 16:55:55 -0300

    actualizadas las querys

--- DataBase/queries.sql
+++ DataBase/queries.sql
@@ -3,7 +3,7 @@
 -- name: CreateEquipo :one
 INSERT INTO equipos (nombre) 
 VALUES ($1) 
-RETURNING *;
+RETURNING *; --Devuelve el id asiginado del nuevo equipo creado
 
 -- name: GetEquipo :one
 SELECT * FROM equipos 
@@ -28,13 +28,14 @@ WHERE id = $1;
 -- name: CreatePartido :one
 INSERT INTO partidos (liga_id, equipo_local_id, equipo_visitante_id, goles_local, goles_visitante, estado)
 VALUES ($1, $2, $3, $4, $5, $6) 
-RETURNING *;
+RETURNING *; --Devuelve el id asiginado del nuevo partido creado
 
 -- name: GetPartido :one
 SELECT * FROM partidos 
 WHERE id = $1;
 
 -- name: ListPartidos :many
+-- lista todos los partidos del dia sin importar la liga a la que pertezcan
 SELECT * FROM partidos 
 ORDER BY id DESC;
 
@@ -50,30 +51,39 @@ DELETE FROM partidos
 WHERE id = $1;
 
 
--- FUNCIONES ESPECIFICAS DE LA APP 
+-- CRUD PARA LA TABLA PRINCIPAL: LIGAS
 
 -- name: CreateLiga :one
 INSERT INTO ligas (nombre, pais) 
-VALUES ($1, $2) RETURNING *;
+VALUES ($1, $2) 
+RETURNING *; --Devuelve el id asiginado de la nueva liga creada
 
--- name: CreatePartidoProgramado :one
-INSERT INTO partidos (liga_id, equipo_local_id, equipo_visitante_id)
-VALUES ($1, $2, $3) RETURNING *;
-
--- name: ActualizarResultadoPartido :exec
-UPDATE partidos 
-SET goles_local = $2, goles_visitante = $3, estado = 'Finalizado'
+-- name: GetLiga :one
+SELECT * FROM ligas
 WHERE id = $1;
 
--- name: ListarPartidosPorLiga :many
+-- name: ListLigas :many
+SELECT * FROM ligas
+ORDER BY nombre ASC;
+
+-- name: ListPartidosLiga :many
+-- lista los partidos de una liga en especifico, sin importar el estado del partido
 SELECT * FROM partidos 
 WHERE liga_id = $1;
 
--- name: InicializarEquipoEnTabla :one
-INSERT INTO tabla_posiciones (liga_id, equipo_id)
-VALUES ($1, $2) RETURNING *;
+-- name: DeleteLiga :exec
+DELETE FROM ligas
+WHERE id = $1;
 
--- name: ActualizarEstadisticas :exec
+
+-- CRUD PARA LA TABLA PRINCIPAL: TABLA DE POSICIONES
+
+-- name: CreateTabla :one
+INSERT INTO tabla_posiciones (liga_id, equipo_id)
+VALUES ($1, $2) 
+RETURNING *; --Devuelve el id de la fila creada en la tabla de posiciones (todos los ids de cada componente y las estadisticas iniciales en 0)
+
+-- name: UpdateTabla :exec
 UPDATE tabla_posiciones 
 SET puntos = puntos + $3, 
     partidos_jugados = partidos_jugados + 1,
@@ -82,9 +92,14 @@ SET puntos = puntos + $3,
     partidos_perdidos = partidos_perdidos + $6
 WHERE liga_id = $1 AND equipo_id = $2;
 
--- name: ObtenerTablaDePosiciones :many
+-- name: GetTabla :many
 SELECT e.nombre as equipo, t.puntos, t.partidos_jugados, t.partidos_ganados, t.partidos_empatados, t.partidos_perdidos
 FROM tabla_posiciones t
 JOIN equipos e ON t.equipo_id = e.id
 WHERE t.liga_id = $1
 ORDER BY t.puntos DESC;
+
+-- name: DeleteEquipoDeTabla :exec
+-- elimina un equipo de la tabla de posiciones de una liga en especifico
+DELETE FROM tabla_posiciones
+WHERE liga_id = $1 AND equipo_id = $2;
